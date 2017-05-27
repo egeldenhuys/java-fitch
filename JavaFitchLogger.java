39b0abbfb579738c18e670a69d466832c51980bc
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-05-27 09:42:29 +0200

    Add ERROR level logging

--- JavaFitchLogger.java
+++ JavaFitchLogger.java
@@ -1,24 +1,41 @@
 public class JavaFitchLogger {
 
     public static final int DEBUG = 0;
+    public static final int ERROR = 1;
+    public static final int FATAL = 2;
+    public static final int INFO = 3;
+    public static final int WARN = 4;
+
     public int logLevel = 0;
+    public String name = "";
 
     public JavaFitchLogger(int defaultLevel) {
         logLevel = defaultLevel;
     }
 
+    public JavaFitchLogger(String n, int defaultLevel) {
+        logLevel = defaultLevel;
+        name = n;
+    }
+
     public void log(int level, String msg) {
-        if (logLevel == level) {
-            if (level == DEBUG) {
-                int callersLineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
-                logTemplate(level, msg, callersLineNumber);
-            }
+        if (logLevel <= level) {
+            int callersLineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
+            logTemplate(level, msg, callersLineNumber);
         }
     }
 
     public void logTemplate(int level, String msg, int line) {
+        // TODO: Use a map?
+
+        String typeName = "";
+
         if (level == DEBUG) {
-            System.out.println("[DEBUG][" + line + "] " + msg);
+            typeName = "DEBUG";
+        } else if (level == ERROR) {
+            typeName = "\u001B[31mERROR\u001B[39;49m";
         }
+
+        System.out.println("[" + typeName + "][" + line + "][" + name + "] " + msg);
     }
 }
