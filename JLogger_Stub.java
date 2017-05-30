05fc24b50a7f9b3260c30ceef2fb42454c8a37a3
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-05-30 12:08:09 +0200

    Add new signatures to stub

--- JLogger_Stub.java
+++ JLogger_Stub.java
@@ -7,6 +7,8 @@ public class JLogger {
     public static final int INFO = 3;
     public static final int WARN = 4;
     public static int logLevel = 0;
+    public static void log(int level, boolean msg) { }
     public static void log(int level, String msg) { }
+    public static void log(int level, int msg) { }
     public static void logTemplate(int level, String msg, int line) { }
 }
